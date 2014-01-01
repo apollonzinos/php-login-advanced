6b9e5a7c3a36aceeb5d77842e31295b092929d1c
Graham Campbell <graham@mineuk.com>
2014-01-01 12:21:15 +0000

    Coding style fixes

--- views/password_reset.php
+++ views/password_reset.php
@@ -2,22 +2,22 @@
 
 <?php if ($login->passwordResetLinkIsValid() == true) { ?>
 <form method="post" action="password_reset.php" name="new_password_form">
-	<input type='hidden' name='user_name' value='<?php echo $_GET['user_name']; ?>' />
-	<input type='hidden' name='user_password_reset_hash' value='<?php echo $_GET['verification_code']; ?>' />
+    <input type='hidden' name='user_name' value='<?php echo $_GET['user_name']; ?>' />
+    <input type='hidden' name='user_password_reset_hash' value='<?php echo $_GET['verification_code']; ?>' />
 
-	<label for="user_password_new"><?php echo WORDING_NEW_PASSWORD; ?></label>
-	<input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+    <label for="user_password_new"><?php echo WORDING_NEW_PASSWORD; ?></label>
+    <input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
 
-	<label for="user_password_repeat"><?php echo WORDING_NEW_PASSWORD_REPEAT; ?></label>
-	<input id="user_password_repeat" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-	<input type="submit" name="submit_new_password" value="<?php echo WORDING_SUBMIT_NEW_PASSWORD; ?>" />
+    <label for="user_password_repeat"><?php echo WORDING_NEW_PASSWORD_REPEAT; ?></label>
+    <input id="user_password_repeat" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
+    <input type="submit" name="submit_new_password" value="<?php echo WORDING_SUBMIT_NEW_PASSWORD; ?>" />
 </form>
 <!-- no data from a password-reset-mail has been provided, so we simply show the request-a-password-reset form -->
 <?php } else { ?>
 <form method="post" action="password_reset.php" name="password_reset_form">
-	<label for="user_name"><?php echo WORDING_REQUEST_PASSWORD_RESET; ?></label>
-	<input id="user_name" type="text" name="user_name" required />
-	<input type="submit" name="request_password_reset" value="<?php echo WORDING_RESET_PASSWORD; ?>" />
+    <label for="user_name"><?php echo WORDING_REQUEST_PASSWORD_RESET; ?></label>
+    <input id="user_name" type="text" name="user_name" required />
+    <input type="submit" name="request_password_reset" value="<?php echo WORDING_RESET_PASSWORD; ?>" />
 </form>
 <?php } ?>
 
